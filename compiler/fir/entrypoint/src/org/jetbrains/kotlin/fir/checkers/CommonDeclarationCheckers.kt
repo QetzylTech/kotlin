f4294a9755baadd24a246931d26cf67f5bebd691
Jinseong Jeon <jsjeon@google.com>
2021-02-16 14:35:03 -0800

    FIR checker: introduce *_NOT_IMPLEMENTED override checker

--- compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/checkers/CommonDeclarationCheckers.kt
+++ compiler/fir/entrypoint/src/org/jetbrains/kotlin/fir/checkers/CommonDeclarationCheckers.kt
@@ -38,6 +38,7 @@ object CommonDeclarationCheckers : DeclarationCheckers() {
 
     override val classCheckers: Set<FirClassChecker> = setOf(
         FirOverrideChecker,
+        FirNotImplementedOverrideChecker,
         FirThrowableSubclassChecker,
         FirOpenMemberChecker,
     )
